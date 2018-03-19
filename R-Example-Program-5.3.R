99ad0a9fd047ac403ba676046e2e557b68816bd9
kia <kia@thinkcenter.bu.edu>
2018-03-19 15:57:31 -0400

    changed the panel.correlation for pairs plot

--- R-Example-Program-5.3.R
+++ R-Example-Program-5.3.R
@@ -15,18 +15,32 @@ data <-data.frame(poverty, metro_res, white, hs_grad, female_house)
 
 ## put correlations on the upper panels,
 ## with size proportional to the correlations.
-panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
+
+
+panel.cor <- function(x, y, digits=2, prefix="", cex.cor) 
 {
-  usr <- par("usr"); on.exit(par(usr))
-  par(usr = c(0, 1, 0, 1))
-  r <- cor(x, y)
-  txt <- format(c(r, 0.123456789), digits = digits)[1]
-  # txt <- paste0(prefix, txt)
+  usr <- par("usr"); on.exit(par(usr)) 
+  par(usr = c(0, 1, 0, 1)) 
+  r <- abs(cor(x, y)) 
+  txt <- format(c(r, 0.123456789), digits=digits)[1] 
+  txt <- paste(prefix, txt, sep="") 
   
-  if(missing(cex.cor)) cex.cor <- 0.8/strwidth(txt)
-  text(0.5, 0.5, txt, cex = cex.cor * r)
+  if(missing(cex.cor)) 
+    cex <- 0.8/strwidth(txt) 
+  
+  test <- cor.test(x,y) 
+  # borrowed from printCoefmat
+  Signif <- symnum(test$p.value, corr = FALSE, na = FALSE, 
+                   cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                   symbols = c("***", "**", "*", ".", " ")) 
+  
+  text(0.5, 0.5, txt, cex = cex * r) 
+  text(.8, .8, Signif, cex=cex, col=2) 
 }
+
 
 pairs(data, upper.panel = panel.cor)
 
+# See read the five ways to visualize pairwise comparisions
+# https://www.r-bloggers.com/five-ways-to-visualize-your-pairwise-comparisons/
 
