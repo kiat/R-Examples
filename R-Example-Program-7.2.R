35441ab506d7fc40409a98e726be1218e43b5c20
kia <kia@thinkcenter.bu.edu>
2018-03-29 16:56:04 -0400

    minor changes

--- R-Example-Program-7.2.R
+++ R-Example-Program-7.2.R
@@ -15,8 +15,6 @@ model <- lm(Energy~PreStretch + AnkleWeights + PreStretch * AnkleWeights, data=e
 # You will get the same with the following model code. 
 # model <- lm(Energy ~ PreStretch * AnkleWeights, data=exercise)
 # model <- lm(Energy ~ PreStretch + AnkleWeights + PreStretch:AnkleWeights, data=exercise)
-# test the following command as well. 
-# model <- lm(Energy~PreStretch:AnkleWeights, data=exercise) 
 
 summary(model)
 Anova(model, type=3)
