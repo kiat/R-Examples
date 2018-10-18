a2530ddb049d2b690a12872e4ce3d8a476ca4249
Kia Teymourian <kiat@dhcp-wifi-8021x-155-41-71-119.bu.edu>
2018-10-18 15:48:49 -0400

    taxi example updated

--- Taxi_Data_Linear_Regression.R
+++ Taxi_Data_Linear_Regression.R
@@ -7,7 +7,7 @@
 # green taxi data is smaller than yellow taxis. 
 # For example this one https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2017-01.csv 
 
-taxi <- read.csv("./Datasets/green_tripdata_2017-01.csv" )
+taxi <- read.csv("./Datasets/green_tripdata_2017-01.csv")
 
 head(taxi, n=4)
 
@@ -16,23 +16,23 @@ head(taxi, n=4)
 # filter the taxi data 
 # we want to have only trip records that the total amoint is between 15 and 50 dollar 
 # and trip distance is 
-taxi<-taxi[ which(taxi$total_amount > 15 & taxi$total_amount < 50 & taxi$trip_distance > 5 & taxi$trip_distance < 20), ]
+taxi<-taxi[ which(taxi$fare_amount > 5 & taxi$fare_amount < 50 & taxi$trip_distance > 5 & taxi$trip_distance < 20), ]
 
 # taxi<-taxi[taxi$total_amount < 200, ]
 # taxi<-taxi[taxi$trip_distance > 5, ]
 # taxi<-taxi[taxi$trip_distance < 50, ]
-#  &&  taxi$total_amount<200 && taxi$trip_distance > 50 && taxi$trip_distance<50 
+#  &&  taxi$fare_amount<200 && taxi$trip_distance > 50 && taxi$trip_distance<50 
 
 # find the correlation 
-cor(taxi$total_amount , taxi$trip_distance)
+cor(taxi$fare_amount , taxi$trip_distance)
 
 # plot the scatterplot 
-plot(taxi$total_amount~taxi$trip_distance)
+plot(taxi$fare_amount~taxi$trip_distance)
 
-my.model<-lm(taxi$total_amount~taxi$trip_distance)
+my.model<-lm(taxi$fare_amount~taxi$trip_distance)
 
 print(my.model)
 
 abline(my.model, col="blue")
 
-boxplot(taxi$total_amount)
+boxplot(taxi$fare_amount)
