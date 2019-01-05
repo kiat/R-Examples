f6bb40b631b7c896c6204b994001518c30ed88d2
Kia Teymourian <kiat@bu.edu>
2019-01-05 17:46:38 -0500

    removed 6 from it

--- Module-1-Example-5.R
+++ Module-1-Example-5.R
@@ -18,32 +18,6 @@ region.y <- c(0, yvalues, 0)
 
 # print out the region.x to see what is inside. 
 region.x
-
 region.y
 
 polygon(region.x, region.y, col="navy")
-
-
-# Define a vector
-curve(dnorm(x), xlim=c(-3,3), main='Normal Density')
-
-# To shade the region represented by P(-3 < X < -2). 
-
-# The first vertex we want for our polygon is (-3,0). 
-cord.x <- c(-3)
-cord.y <- c(0)
-
-# The 2nd vertex will be (-3, f(-3)), f(-3) is the normal density evaluated at -3. 
-cord.x <- c(cord.x, -3) 
-cord.y <- c(cord.y, dnorm(-3)) 
-
-# The 3rd and 4th vertices are (-2,f(-2)) and (-2,0)
-cord.x <- c(cord.x, -2, -2)
-cord.y <- c(cord.y, dnorm(-2), 0)
-
-# Now, we can use the polygon function and color parts in skyblue color. 
-polygon(cord.x, cord.y, col="skyblue")
-
-
-
-
