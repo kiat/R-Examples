7c110915a615412f178f5e4be18e35f0232cbb2f
Kia Teymourian <kiat@users.noreply.github.com>
2019-02-25 16:43:37 -0500

    Sample Tests for Proportions

--- Module-6-Example-2.R
+++ Module-6-Example-2.R
@@ -1,78 +1,46 @@
-# install.packages("aod")
-# install.packages("pROC")
-
-library(aod)
-library(stats)
-library(pROC)
+# One and Two Sample Tests for Proportion
 
 
+# One Sample Tests for a Proportion
+
+# Example:
+# We are interested in estimating the proportion of children in the
+# county that are vaccinated for measles. We suspect that it may be
+# as low as 80%. A random sample is taken of 100 children from the
+# county. Of those sampled, only 70 were vaccinated. Formally test
+# if the proportion of vaccinated children is different than 80%.
+
+# See the manual page for prop.test() function 
+?prop.test
+
+# One Sample Tests for a Proportion 
+prop.test (70 , 100 , p=0.8 , conf.level=0.95 , correct=FALSE)
 
 
-data<-read.csv("Datasets/cevent.csv")
-
-attach(data)
-# print a small part of the data 
-head(data)
+# One Sample Tests for a Proportion with continuity correction 
+prop.test (70 , 100 , p=0.8 , conf.level=0.95 , correct=TRUE)
 
 
-# fit a logistic regeression 
-m <- glm(data$event ~ data$chol, family = binomial) 
+# Two-Sample Tests for Proportions
 
-summary(m)
+# Example:
+# An investigator is interested in the long-term effects of preschool
+# programs on low-income children. A study was conducted where by two
+# groups of children were followed over time. The first group of 61
+# children did not attend preschool. The second group of 62 children
+# (from similar areas and with similar backgrounds of those in the
+# first sample) attended preschool as 3- and 4-year-olds. The need
+# for social programs as adults was the outcome of interest. Of the
+# group who did not attend preschool, 49 of them needed social
+# services (mainly welfare) between the ages of 18 and 30. In the
+# preschool group, 38 required social services in the same age range.
 
-# ORs per 1 unit increase 
-# same as calculation by hand (OR): exp(0.02119)
-exp(cbind(OR = coef(m), confint.default(m)))
+# Two Sample Test for Proportion 
+prop.test(c(49, 38) , c(61, 62) , conf.level =0.95 , correct=FALSE)
 
-
-
-# OR per 10 unit increase 
-# m$coefficients gives me access to coefficient values
-exp(m$coefficients[2]*10)
-exp((m$coefficients[2]-qnorm(0.975)*summary(m)$coefficients[2,2])*10)
-exp((m$coefficients[2]+qnorm(0.975)*summary(m)$coefficients[2,2])*10)
-
-# predict risk for each patient
-risk <-predict(m, type=c("response"))
-
-risk 
-
-
-# predict risk for patient with chol of 190: exp(-3.12716+0.02119*190)/(1+exp(-3.12716+0.02119*190))
-risk[41]
-exp(m$coefficients[1] + m$coefficients[2]*190) / (1+exp(m$coefficients[1] + m$coefficients[2]*190))
-
-
-
-# multiple logistic regression 
-data$male <- ifelse(data$sex =="M", 1, 0)
-m2<-glm(data$event ~ data$chol + data$male + data$age, family=binomial)
-summary(m2)
-
-# Global test 
-# install.package("aod")
-library(aod)
-
-wald.test(b=coef(m2), Sigma = vcov(m2), Terms = 2:4)
-
-# ORs per 1 unit increase 
-exp(cbind(OR = coef(m2), confint.default(m2)))
-
-# ROC curve 
-# install.package("pROC")
-library(pROC)
-
-# using model with chol and sex and age 
-data$prob <-predict(m2, type=c("response"))
-
-# ROC Curve 
-g <- roc(data$event ~ data$prob)
-print(g)
-
-plot(g)
+# Two Sample Test for Proportion with Continuity Correction
+prop.test(c(49, 38) , c(61, 62) , conf.level =0.95 , correct=TRUE)
 
 
 
-plot(1- g$specificities, g$sensitivities, type="l", xlab="1-Specifity", ylab="Sensivity", main="ROC Curve")
-abline(a=0, b=1)
-grid()
+
