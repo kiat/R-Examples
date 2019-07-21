f7f721d05bc606469989fb9562b1bda3255f88e7
Kia Teymourian <kiat@bu.edu>
2019-07-21 12:56:33 -0400

    Updated M2-Example-3 to demonstrate estimation of df in two-sample t.test

--- Module-2-Example-3.R
+++ Module-2-Example-3.R
@@ -23,32 +23,55 @@ decay
 # Print out a summary of the data for the 2 weeks sample data 
 summary(decay$strength[decay$weeks==2])
 
-
 # Print out a summary of the data for the 16 weeks sample data 
 summary(decay$strength[decay$weeks==16])
+
+# Or you can do 
+aggregate(decay$strength, by =list(as.factor(decay$weeks)), FUN=summary)
+aggregate(decay$strength, by =list(as.factor(decay$weeks)), FUN=sd)
 
 # read the manual of t-test function 
 ?t.test 
 
 # Compute the test t statistic and the associated p-value
 # Reject H0 if p <= alph, p<=0.1
-ttest1 <- t.test(decay$strength[decay$weeks==2], decay$strength[decay$weeks==16], alternative="greater", conf.level=0.9)
+t.test(decay$strength[decay$weeks==2], decay$strength[decay$weeks==16], alternative="greater", conf.level=0.9)
 
-print(ttest1)
 
 # p-value = 0.1857
 
 # Using higher confidence level of 0.95
 # Two Sample t-test
-ttest2 <- t.test(decay$strength[decay$weeks==2], decay$strength[decay$weeks==16], alternative="two.sided", conf.level=0.95)
+t.test(decay$strength[decay$weeks==2], decay$strength[decay$weeks==16], alternative="two.sided", conf.level=0.95)
 
-print(test2)
-
+# the same command as above because of default variables. 
+t.test(decay$strength[decay$weeks==2], decay$strength[decay$weeks==16])
 # alternative hypothesis: true difference in means is not equal to 0
 # 95 percent confidence interval:
 # -12.2789  27.0789
 
+#  90% confidence interval
+t.test(decay$strength[decay$weeks==2], decay$strength[decay$weeks==16], alternative="two.sided", conf.level=0.90)
+
+# 90 percent confidence interval:
+# -7.932851 22.732851
+# We are 90% confident that the mean difference in breaking strengths is between -7.932851 and 22.732851
+
+# Manual implementation of 
+
+mean.diff <- (mean(decay$strength[decay$weeks==2]) -  mean(decay$strength[decay$weeks==16])) 
+minLeanght <- min(length(decay$strength[decay$weeks==2]), length(decay$strength[decay$weeks==16]))
+
+margin.Off.error <- qt(0.05 , df = 4, lower.tail = F ) * sqrt( sd(decay$strength[decay$weeks==2])^2 / minLeanght  +  sd(decay$strength[decay$weeks==16])^2 / minLeanght    )
+
+lower.bound <- mean.diff - margin.Off.error
+upper.bound <- mean.diff + margin.Off.error
+
+# Conculsion: 90% mean difference in breaking strengths is between -8.56 and 23.36
+# This is a good correct estimate of the above 
+
+
 # return value has some contents 
-names(ttest2)
+# names(ttest2)
 
 
