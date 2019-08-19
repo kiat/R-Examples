ccfc69edd745335c5dd5ef8ec37289f4e298e587
Kia Teymourian <kiat@bu.edu>
2019-08-19 17:21:27 -0400

    added a new example to illustrate Logistic Regression results

--- Module-6-Example-7.R
+++ Module-6-Example-7.R
@@ -1,27 +1,82 @@
 # setwd("SET THE Working Director to THE PATH TO THIS DIRECTORY")
 rm(list = ls())
+
+# https://www.r-bloggers.com/evaluating-logistic-regression-models/
+
 library(aod)
 library(stats)
 library(pROC)
 
-data<-read.csv("Datasets/cevent.csv")
-data$male <- ifelse(data$sex =="M", 1, 0)
+library(survey)
 
-m <- glm(event ~ chol + age , data=data, family = "binomial")
+# Heart disease is a comprehensive disease that many factors can contribute to its morbidity.
+# To figure out the relationship between some common factors or heart related factors and the incidence, a study group set the Cleverland database of about 300 
+# people about their sex, age and so on, and whether they have heart disease.
+# First they want to know whether there are some common factors that influence the morbidity.
+
+# Link to original data set https://archive.ics.uci.edu/ml/datasets/Heart+Disease
+# The link to the original data:  https://www.kaggle.com/ronitf/heart-disease-uci/version/1
+
+data<-read.csv("Datasets/heart-disease.csv")
+
+head(data)
+
+# Now let us separate the data set into two parts of train and test. 
+
+## 75% of the sample size
+smp_size <- floor(0.75 * nrow(data))
+
+## set the seed to make your partition reproducible
+set.seed(123)
+train_ind <- sample(seq_len(nrow(data)), size = smp_size)
+
+train.Data <- data[train_ind, ]
+test.Data <- data[-train_ind, ]
+
+
+m <- glm(heart.target ~ heart.age  + heart.sex + heart.chol,   data=train.Data, family = "binomial")
 summary(m)
 
 
-# http://r-statistics.co/Logistic-Regression-With-R.html
 # Find the optimal cutOff
 # Default cutoff is 0.5
 # Chaning the cutoff can improve the accuracy 
-data$prob <-predict(m, type=c("response"))
+# prob <-predict(m, type=c("response"))
+predictValues <- predict(m , newdata = test.Data,  type = "response")
+# log_predict <- predict(m , newdata = test.Data)
+
+
+final.prediction <- ifelse(predictValues > 0.5, 1, 0)
+cm <- confusionMatrix(test.Data$heart.target, final.prediction)
+print(cm)
+# Or you can go directly Like following
+# cm <- confusionMatrix(test.Data$heart.target, predictValues,   threshold = 0.5)
+
+
+
+#plot ROC
+library(ROCR)
+library(Metrics)
+pr <- prediction(predictValues,  test.Data$heart.target)
+perf <- performance(pr,measure = "tpr",x.measure = "fpr")
+plot(perf)
+auc(test.Data$heart.target, predictValues)
+
 
 # The InformationValue::optimalCutoff function provides ways to find 
 # the optimal cutoff
 
 # install.packages("InformationValue")
 library(InformationValue)
-optCutOff <- optimalCutoff(data$prob, m)[1] 
+optCutOff <- optimalCutoff(actuals=test.Data$heart.target, predictedScores=predictValues, optimiseFor="Both", returnDiagnostics=TRUE)
+
+optCutOff$optimalCutoff
+# optCutOff$optimalCutoff
+# [1] 0.5322385
+
+
+
+
+
 
 
