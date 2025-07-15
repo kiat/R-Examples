a4682f23b60662abf4f3edcf03ca8ea605461e24
Kia Teymourian <kiat@users.noreply.github.com>
2025-07-15 12:29:39 -0500

    update

--- Module-1-Example-1.R
+++ Module-1-Example-1.R
@@ -10,6 +10,48 @@ days <- read.csv("Datasets/Example_Data.csv", header = FALSE)
 my.days <- unlist(days, use.names = FALSE)
 
 
+# Print basic statistics
+
+mean (my.days)
+median (my.days )
+min (my.days )
+max ( my.days )
+quantile ( my.days )
+var( my.days )
+sd( my.days )
+
+# Summary
+summary(my.days)
+
+# Create a boxplot
+boxplot(my.days,
+        main = "Boxplot of Data",
+        ylab = "Values",
+        col = "lightblue",
+        border = "darkblue")
+
 # my.days is now a simple vector data type that I can work with. 
 # generate a histogram form data 
 hist(my.days)
+
+
+
+# Create a histogram
+hist(my.days,
+     main = "Histogram of Data",
+     xlab = "Values",
+     col = "lightgreen",
+     border = "black",
+     breaks = 10)
+
+# Create a vector using seq function. 
+break.points <- seq(from = 10, to = 36, by = 2)
+
+# Create a histogram
+hist(my.days,
+     main = "Histogram of Data",
+     xlab = "Values",
+     col = "lightgreen",
+     border = "black",
+     breaks = break.points)
+
